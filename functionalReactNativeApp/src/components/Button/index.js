765e18f51bc4956cec589ee179b85ae0ab1fd371
Omer <teouysal@outlook.com>
2022-12-20 22:50:59 +0300

    updated button component

--- functionalReactNativeApp/src/components/Button/index.js
+++ functionalReactNativeApp/src/components/Button/index.js
@@ -1,11 +1,10 @@
-import {View, Text, TouchableOpacity} from 'react-native';
+import {Text, TouchableOpacity} from 'react-native';
 import React from 'react';
 import {styles} from './style';
 
-const CustomButton = ({title}) => {
-  const handlePress = () => {};
+const CustomButton = ({title, onPress}) => {
   return (
-    <TouchableOpacity style={styles.container} onPress={handlePress}>
+    <TouchableOpacity style={styles.container} onPress={onPress}>
       <Text style={styles.title}>{title}</Text>
     </TouchableOpacity>
   );
--- functionalReactNativeApp/src/components/Button/style.js
+++ functionalReactNativeApp/src/components/Button/style.js
@@ -1,15 +1,17 @@
 import {StyleSheet} from 'react-native';
+import {colors} from '../../utils/colors';
 
 export const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#4F63AC',
+    backgroundColor: colors.blue,
     padding: 8,
     paddingVertical: 20,
     paddingHorizontal: 8,
     borderRadius: 8,
+    width: '100%',
   },
   title: {
-    color: '#FFFFFF',
+    color: colors.white,
     textAlign: 'center',
     fontSize: 16,
     fontWeight: 'bold',
