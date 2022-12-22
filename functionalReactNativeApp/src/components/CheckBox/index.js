7835dda7906511f00167be6608b0692f8fd21172
Omer <teouysal@outlook.com>
2022-12-22 22:03:53 +0300

    added checkbox component

---
+++ functionalReactNativeApp/src/components/CheckBox/index.js
@@ -0,0 +1,23 @@
+import {Image, View, TouchableOpacity} from 'react-native';
+import React from 'react';
+import {styles} from './style';
+
+const CheckBox = ({checked, onChecked}) => {
+  console.log('check box');
+  return (
+    <TouchableOpacity
+      style={styles.container}
+      onPress={() => onChecked(pr => !pr)}>
+      {checked && (
+        <View style={styles.innerContainer}>
+          <Image
+            style={styles.checkIcon}
+            source={require('../../assests/white-check.png')}
+          />
+        </View>
+      )}
+    </TouchableOpacity>
+  );
+};
+
+export default React.memo(CheckBox);
