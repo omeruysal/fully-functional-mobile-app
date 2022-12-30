0d4fcccb1693574d785d915ccaf5882c26f016ba
Omer <teouysal@outlook.com>
2022-12-30 09:43:55 +0300

    added authheader

---
+++ functionalReactNativeApp/src/components/AuthHeader/index.js
@@ -0,0 +1,19 @@
+import {View, Text, Pressable, Image} from 'react-native';
+import React from 'react';
+import {styles} from './style';
+
+const AuthHeader = ({title, onBackPress}) => {
+  return (
+    <View style={styles.container}>
+      <Pressable onPress={onBackPress}>
+        <Image
+          style={styles.image}
+          source={require('../../assests/auth-back.png')}
+        />
+      </Pressable>
+      <Text style={styles.title}>{title}</Text>
+    </View>
+  );
+};
+
+export default AuthHeader;
