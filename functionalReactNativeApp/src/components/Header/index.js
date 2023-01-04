ae452a381853b88df880eb31155b514ff1f88b91
Omer <teouysal@outlook.com>
2023-01-04 18:27:41 +0300

    added header component

---
+++ functionalReactNativeApp/src/components/Header/index.js
@@ -0,0 +1,57 @@
+import {View, Text, Pressable, Image} from 'react-native';
+import React, {useState} from 'react';
+import {styles} from './style';
+import Input from '../Input';
+
+const Header = ({
+  title,
+  onBackPress,
+  onLogout,
+  showLogout,
+  showSearch,
+  showBack,
+}) => {
+  const [showSearchInput, setShowSearchInput] = useState(false);
+  const onSearchClick = () => {
+    setShowSearchInput(s => !s);
+  };
+  return (
+    <View>
+      <View style={styles.container}>
+        {showBack ? (
+          <Pressable onPress={onBackPress}>
+            <Image
+              style={styles.icon}
+              source={require('../../assests/back.png')}
+            />
+          </Pressable>
+        ) : showSearch ? (
+          <Pressable onPress={onSearchClick}>
+            <Image
+              style={styles.icon}
+              source={require('../../assests/search.png')}
+            />
+          </Pressable>
+        ) : (
+          <View style={styles.space} />
+        )}
+
+        <Text style={styles.title}>{title}</Text>
+
+        {showLogout ? (
+          <Pressable onPress={onLogout}>
+            <Image
+              style={styles.icon}
+              source={require('../../assests/logout.png')}
+            />
+          </Pressable>
+        ) : (
+          <View style={styles.space} />
+        )}
+      </View>
+      {showSearchInput ? <Input placeHolder={'Type your keyword...'} /> : null}
+    </View>
+  );
+};
+
+export default React.memo(Header);
